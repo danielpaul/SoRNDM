dac6874979b5572af9358c87ae3ecbeead472656
John <johnarpaul@gmail.com>
2019-11-16 11:22:31 +0000

    added faker gem

--- app/controllers/league_of_legends_controller.rb
+++ app/controllers/league_of_legends_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
 
-  def champion
+  def index
   end
 
-  
+  def champion
+    @value = gen_champion
+  end
+
+  private
+
+  def gen_champion
+    Faker::Name.name
+  end
+
 end
